019c466f2f639d041be07d03cd896d685935cd4d
Dilshan Jagoda <dilshanjagoda2008@gmail.com>
2026-07-26 16:09:07 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   server: {
     allowedHosts: ['clemmie-unsurfeited-contactually.ngrok-free.dev'],
   },
